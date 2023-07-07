8f92293e66e8db6cc68864526f9405409c0f675d
SoumyaAchar0804 <120330832+SoumyaAchar0804@users.noreply.github.com>
2023-07-07 17:38:53 +0530

    Update InsureMeApplicationTests.java

--- src/test/java/com/project/insurance/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/insurance/insureme/InsureMeApplicationTests.java
@@ -3,11 +3,9 @@ package com.project.insurance.insureme;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 
 class InsureMeApplicationTests {
